f8c2fb92a793a16fa129ff56e65091a1ab155ec5
lavet13 <lavet13@mail.ru>
2023-08-16 15:48:34 +0300

    okey

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,22 @@
 <h1>Hello Cloudreach!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
+// $host = 'scp108.hosting.reg.ru';
+// $user = 'u0921714_work';
+// $pass = 'FrwB2.K7xfLi';
+// $conn = new mysqli($host, $user, $pass);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL successfully!";
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } else {
+//     echo "Connected to MySQL successfully!";
+// }
+
+try {
+    $db = new PDO("mysql:dbname=u0921714_calc;host=scp108.hosting.reg.ru","u0921714_work","FrwB2.K7xfLi");
+    echo 'connected!';
+} catch(PDOException $e){
+    echo $e;
+	die('Ошибка подключения к серверу баз данных.');
 }
-
 ?>
